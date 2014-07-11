c4907e5a2f5d67a820ee9fb7967f74fc23e1de56
oOChainLynxOo <chainlynx96@gmail.com>
2014-07-11 19:36:19 -0400

    Removed strings
    on_use = minetest.item_eat(2) and ground_content = true

--- init.lua
+++ init.lua
@@ -1,8 +1,6 @@
 minetest.register_node("crackedstone:cracked_stone", {
 	description = "Cracked Stone",
 	tiles = {"cracked_stone.png"},
-	on_use = minetest.item_eat(2),
-	is_ground_content = true,
 	groups = {cracky=3},
 	paramtype="light",
 })
